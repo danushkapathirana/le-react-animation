0c2aff08203f64dbb4e94522ddbaf5f50ed07645
Danushka Pathirana <pathirana.danushka.rp@gmail.com>
2023-04-28 19:16:14 +0530

    identify the limitations of animations with react

--- src/App.js
+++ src/App.js
@@ -21,8 +21,8 @@ const App = () => {
     <div className="App">
       <h1>React Animations</h1>
 
-      <Modal show={showModal} closed={closeModalHandler} />
-      <Backdrop show={showModal}  />
+      {showModal ? <Modal show={showModal} closed={closeModalHandler} /> : null}
+      {showModal ? <Backdrop show={showModal}  /> : null}
 
       <button className="Button" onClick={openModalHandler}>Open Modal</button>
       <h3>Animating Lists</h3>
@@ -33,3 +33,14 @@ const App = () => {
 }
 
 export default App
+
+/**
+ * - limitations of css animations with React -
+ * 
+ * - in the developer tools modal and backdrop are always visible
+ * - because its visibility control over the css classes, it's not affected to the DOM tree
+ * 
+ * - therefore, we have to use conditional rendering
+ * - when use conditional rendering close animation does not work
+ * - because App.js does not wait to re-render this JSX until finish the animation
+ */
